fac66d0657f0fb0fe499a229623e5f2a654dbf6a
vtran2535 <72771504+vtran2535@users.noreply.github.com>
2021-05-07 21:39:18 -0700

    Add files via upload

---
+++ memmgr.c
@@ -0,0 +1,81 @@
+//
+//  memmgr.c
+//  memmgr
+//
+//  Created by William McCarthy on 17/11/20.
+//  Copyright © 2020 William McCarthy. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define ARGC_ERROR 1
+#define FILE_ERROR 2
+#define BUFLEN 256
+#define FRAME_SIZE  256
+
+
+//-------------------------------------------------------------------
+unsigned getpage(unsigned x) { return (0xff00 & x) >> 8; }
+
+unsigned getoffset(unsigned x) { return (0xff & x); }
+
+void getpage_offset(unsigned x) {
+  unsigned  page   = getpage(x);
+  unsigned  offset = getoffset(x);
+  printf("x is: %u, page: %u, offset: %u, address: %u, paddress: %u\n", x, page, offset,
+         (page << 8) | getoffset(x), page * 256 + offset);
+}
+
+int main(int argc, const char* argv[]) {
+  FILE* fadd = fopen("addresses.txt", "r");    // open file addresses.txt  (contains the logical addresses)
+  if (fadd == NULL) { fprintf(stderr, "Could not open file: 'addresses.txt'\n");  exit(FILE_ERROR);  }
+
+  FILE* fcorr = fopen("correct.txt", "r");     // contains the logical and physical address, and its value
+  if (fcorr == NULL) { fprintf(stderr, "Could not open file: 'correct.txt'\n");  exit(FILE_ERROR);  }
+
+  char buf[BUFLEN];
+  unsigned   page, offset, physical_add, frame = 0;
+  unsigned   logic_add;                  // read from file address.txt
+  unsigned   virt_add, phys_add, value;  // read from file correct.txt
+
+  printf("ONLY READ FIRST 20 entries -- TODO: change to read all entries\n\n");
+
+  // not quite correct -- should search page table before creating a new entry
+      //   e.g., address # 25 from addresses.txt will fail the assertion
+      // TODO:  add page table code
+      // TODO:  add TLB code
+  while (frame < 20) {
+
+    fscanf(fcorr, "%s %s %d %s %s %d %s %d", buf, buf, &virt_add,
+           buf, buf, &phys_add, buf, &value);  // read from file correct.txt
+
+    fscanf(fadd, "%d", &logic_add);  // read from file address.txt
+    page   = getpage(  logic_add);
+    offset = getoffset(logic_add);
+    
+    physical_add = frame++ * FRAME_SIZE + offset;
+    
+    assert(physical_add == phys_add);
+    
+    // todo: read BINARY_STORE and confirm value matches read value from correct.txt
+    
+    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -- passed\n", logic_add, page, offset, physical_add);
+    if (frame % 5 == 0) { printf("\n"); }
+  }
+  fclose(fcorr);
+  fclose(fadd);
+  
+  printf("ONLY READ FIRST 20 entries -- TODO: change to read all entries\n\n");
+  
+  printf("ALL logical ---> physical assertions PASSED!\n");
+  printf("!!! This doesn't work passed entry 24 in correct.txt, because of a duplicate page table entry\n");
+  printf("--- you have to implement the PTE and TLB part of this code\n");
+
+//  printf("NOT CORRECT -- ONLY READ FIRST 20 ENTRIES... TODO: MAKE IT READ ALL ENTRIES\n");
+
+  printf("\n\t\t...done.\n");
+  return 0;
+}
